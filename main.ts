e76e8ee9a2b21de2abf804722d6efdf039fee020
fu1819 <71689568+fu1819@users.noreply.github.com>
2020-11-10 23:29:37 +0800

    從(4，0)開始L行展開

--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
-let x0 = 0
-let y0 = 0
 let x = 0
 let y = 0
+let x0 = 0
+let y0 = 0
+let x1 = 0
+let y1 = 0
 basic.forever(function () {
-    x0 = 4
-    y0 = 0
     for (let I = 0; I <= 4; I++) {
-        led.plot(x0, y0)
-        for (let j = 0; j <= 1; j++) {
-            x = x0 - 1
-            y = y0 + 1
-            led.plot(x, y)
-            basic.pause(500)
+        for (let j = 0; j <= I; j++) {
+            x = 4 - I
+            y = I
+            x0 = x + j
+            y0 = I
+            x1 = 4 - I
+            y1 = I - j
+            led.plot(x0, y0)
+            led.plot(x1, y1)
+            basic.pause(200)
         }
     }
 })
